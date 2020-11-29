12bb2fda284e20cd7518180e50339b38ba27077f
Falkonny <62031372+Falkonny@users.noreply.github.com>
2020-11-29 12:11:20 +0300

    Добавьте файлы проекта.

---
+++ Functional/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Functional
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task_3();
+        }
+
+        public static void Task_1()
+        {
+            var words = Console.ReadLine();
+            Action<string> action = word => { Console.WriteLine(word); };
+            foreach (var word in words.Split(' ')) action(word);
+        }
+
+        public static void Task_2()
+        {
+            var words = Console.ReadLine();
+            Action<string> action = word => { Console.WriteLine(word + "(нет в наличии)"); };
+            foreach (var word in words.Split(' ')) action(word);
+        }
+
+        public static void Task_3()
+        {
+            string[] inputNum = Console.ReadLine()?.Split(' ').ToArray();
+
+            Func<string[], int> minimal = numbers =>
+            {
+                var nums = new int[numbers.Length];
+                for (var i = 0; i < numbers.Length; i++) nums[i] = int.Parse(numbers[i]);
+
+                return nums.Min();
+            };
+
+            Console.WriteLine(minimal(inputNum));
+        }
+
+        public static void Task_4()
+        {
+
+        }
+    }
+}
